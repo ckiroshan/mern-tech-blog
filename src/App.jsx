6efcd1c0a178c73392049de80a7e8a7c73a0687a
ckiroshan <ckiroshan@gmail.com>
2025-03-23 14:35:22 +0530

    feat: Route Post component through Home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import Post from "./components/post";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
     <Routes>
       <Route path={"/"} element={<Layout />}></Route>
-      <Route index element={<Post />} />
+      <Route index element={<Home />} />
     </Routes>
   );
 }
