bf32408771ef42e28bbef5046370cd206815dcfd
ckiroshan <ckiroshan@gmail.com>
2025-03-25 14:50:20 +0530

    feat(auth): implement Register form with state management

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
+
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleFormSubmit(e) {
+    e.preventDefault();
+    await fetch(`http://localhost:8021/register`, {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+  }
   return (
-    <form className="register">
+    <form className="register" onSubmit={handleFormSubmit}>
       <h1>Register</h1>
-      <input type="text" placeholder="username" />
-      <input type="password" placeholder="password" />
+      <input type="text" placeholder="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+      <input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
       <button>Register</button>
     </form>
   );
