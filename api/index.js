ce8fa0e834b40a56fe411ce784980d5d9c01e5d0
ckiroshan <ckiroshan@gmail.com>
2025-03-25 14:48:51 +0530

    feat(api): add mock /register endpoint that echoes payload

--- api/index.js
+++ api/index.js
@@ -2,10 +2,13 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-app.use(cors());
 
-app.get("/test", (req, res) => {
-  res.json("test ok2");
+app.use(cors());
+app.use(express.json());
+
+app.post("/register", (req, res) => {
+  const { username, password } = req.body;
+  res.json({ requestData: { username, password } });
 });
 
 app.listen(8021);
