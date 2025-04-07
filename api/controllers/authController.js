277b502ba00feb19fb74387b03abff63bd228b34
ckiroshan <ckiroshan@gmail.com>
2025-04-07 15:28:44 +0530

    feat(api): Hash user passwords using bcrypt for enhanced security

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,10 +1,16 @@
 import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
+const salt = bcrypt.genSaltSync(10);
 
 // Create User
 export const register = async (req, res) => {
   const { username, password } = req.body;
   try {
-    const userDoc = await User.create({ username, password });
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
     res.status(201).json(userDoc);
   } catch (err) {
     res.status(400).json({ error: err.message });
