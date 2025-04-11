46f1506b801ee6f8d110b67c9ed6baa2b5480eeb
ckiroshan <ckiroshan@gmail.com>
2025-04-11 15:42:31 +0530

    feat(auth): create UserContext to manage authentication state across components

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import { UserContextProvider } from "./service/UserContext";
 import "./App.css";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path={"/"} element={<Layout />}></Route>
-      <Route index element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <UserContextProvider>
+      <Routes>
+        <Route path={"/"} element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
   );
 }
