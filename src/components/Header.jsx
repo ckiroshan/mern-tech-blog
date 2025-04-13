dbdb299b39ad48269b2248302c1aa55a30d86be8
ckiroshan <ckiroshan@gmail.com>
2025-04-13 16:17:06 +0530

    feat(ui): make navigation dynamically respond to auth state

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,31 @@
+import React, { useContext } from "react";
 import React from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "../service/UserContext";
 
 const Header = () => {
+  const { userInfo, setUserInfo } = useContext(UserContext);
+
+  const username = userInfo?.username;
+  
   return (
     <header>
       <Link to="/" className="logo">
         iBlog
       </Link>
       <nav>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
+        {username && (
+          <>
+            <Link to="/add-post">Add new post</Link>
+            <a onClick={logout}>Logout</a>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
       </nav>
     </header>
   );
