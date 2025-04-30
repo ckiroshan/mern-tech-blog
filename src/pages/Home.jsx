0b76c967515673c0c0bf33ff06a04cbe472b3369
ckiroshan <ckiroshan@gmail.com>
2025-04-30 10:57:23 +0530

    feat: Integrate Loader component into Home page for loading state

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,32 +2,48 @@ import React, { useContext, useEffect, useState } from "react";
 import Post from "../components/post";
 import { fetchAllPosts } from "../service/api";
 import { UserContext } from "../service/UserContext";
+import Loader from "../components/Loader";
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
   const [filteredPosts, setFilteredPosts] = useState([]);
   const { searchQuery } = useContext(UserContext);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetchAllPosts().then((posts) => {
-      setPosts(posts);
-      setFilteredPosts(posts);
-    });
+    const loadData = async () => {
+      try {
+        const postsData = await fetchAllPosts();
+        setPosts(postsData);
+        setFilteredPosts(postsData);
+      } catch (error) {
+        console.error("Error loading posts:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadData();
   }, []);
 
   useEffect(() => {
-    if (searchQuery) {
-      const lowerCaseQuery = searchQuery.toLowerCase();
-      const filtered = posts.filter((post) => {
-        return post.title.toLowerCase().includes(lowerCaseQuery) || post.summary.toLowerCase().includes(lowerCaseQuery) || post.content.toLowerCase().includes(lowerCaseQuery) || post.author.username.toLowerCase().includes(lowerCaseQuery);
-      });
-      setFilteredPosts(filtered);
-    } else {
-      setFilteredPosts(posts);
+    if (!loading) {
+      if (searchQuery) {
+        const lowerCaseQuery = searchQuery.toLowerCase();
+        const filtered = posts.filter((post) => {
+          return post.title.toLowerCase().includes(lowerCaseQuery) || post.summary.toLowerCase().includes(lowerCaseQuery) || post.content.toLowerCase().includes(lowerCaseQuery) || post.author.username.toLowerCase().includes(lowerCaseQuery);
+        });
+        setFilteredPosts(filtered);
+      } else {
+        setFilteredPosts(posts);
+      }
     }
-  }, [searchQuery, posts]);
+  }, [searchQuery, posts, loading]);
 
-  return <>{filteredPosts.length > 0 ? filteredPosts.map((post) => <Post key={post._id} {...post} />) : <div className="no-results">No posts match your search.</div>}</>;
+  return <>
+  {filteredPosts.length > 0 ? filteredPosts.map((post) => 
+  <Post key={post._id} {...post} />) 
+  : loading ? <Loader loading={loading} /> 
+  : <div className="no-results">{searchQuery ? "No posts match your search." : "No posts found."}</div>}</>;
 };
 
 export default Home;
