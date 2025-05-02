9f6444757a0bfec41f49884c3219bbe98983199c
ckiroshan <ckiroshan@gmail.com>
2025-05-02 08:48:30 +0530

    feat(footer): Add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>Made with ♥ by ckiroshan © {new Date().getFullYear()}</p>
+    </footer>
+  );
+};
+
+export default Footer;
