51e4f302b7f3fa4ea30a69b1ef03e363eb304ca4
ckiroshan <ckiroshan@gmail.com>
2025-05-09 09:18:04 +0530

    feat(api): Enhance CORS policy to support multiple allowed origins & preflight requests

--- api/index.js
+++ api/index.js
@@ -7,10 +7,24 @@ import adminRoutes from "./routes/adminRoutes.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
-const client_URL = "http://localhost:5173";
+const allowedOrigins = ["http://localhost:5173", "https://irobits.netlify.app"];
 
 // Middleware
-app.use(cors({ credentials: true, origin: client_URL }));
+app.use(
+  cors({
+    credentials: true,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.options("*", cors()); // For preflight requests
 app.use(express.json());
 app.use(cookieParser());
 
