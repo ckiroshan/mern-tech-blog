9e6dcd99063b566e9bcc8aeb4b7855aeb16b37fe
ckiroshan <ckiroshan@gmail.com>
2025-06-02 08:30:31 +0530

    docs: Add comments to ProtectedRoute to refine route functionality

--- src/components/routes/ProtectedRoute.jsx
+++ src/components/routes/ProtectedRoute.jsx
@@ -2,19 +2,23 @@ import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { UserContext } from "../../service/UserContext";
 
+// A wrapper to protect routes based on user authentication status
 const ProtectedRoute = ({ children, requireAuth = true }) => {
   const { userInfo } = useContext(UserContext);
 
+  // Redirect to login 
   if (requireAuth && !userInfo) {
-    // Redirect to login if auth is required but user is not logged in
+    // if auth is required but user is not logged in
     return <Navigate to="/login" replace />;
   }
 
+  // Redirect from auth pages (login, register)
   if (!requireAuth && userInfo) {
-    // Redirect from auth pages if already logged in
+    // if user is already logged in
     return <Navigate to="/" replace />;
   }
 
+  // Else, render the child component
   return children;
 };
 
