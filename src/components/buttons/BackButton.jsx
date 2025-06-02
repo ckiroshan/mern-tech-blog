16e8d95b9eb1c65fe98c55a42ca518c914a2ee15
ckiroshan <ckiroshan@gmail.com>
2025-06-02 08:45:05 +0530

    docs: Add comments to Back button

--- src/components/buttons/BackButton.jsx
+++ src/components/buttons/BackButton.jsx
@@ -1,15 +1,16 @@
 import { useNavigate } from "react-router-dom";
 import { FaArrowLeft } from "react-icons/fa";
 
+// Back Button component to go back
 const BackButton = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate(); // Hook to access navigation
 
   return (
     <button
       onClick={() => navigate(-1)} // Go back to previous page
       className="form__button back__button"
     >
-      <FaArrowLeft />
+      <FaArrowLeft /> {/* Back icon */}
       Go Back
     </button>
   );
